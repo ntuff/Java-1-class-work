e6cf5abbc813df3e9d1e8ebf259896abbee732bc
ntuff <ndtuff@gmail.com>
2022-09-20 17:12:30 -0230

    Added account functionality.

--- src/Account.java
+++ src/Account.java
@@ -1,11 +1,33 @@
 public class Account {
     private String name;
+    private double balance;
+
+    public Account(String name, double balance) {this.name = name;
+    if(balance > 0)
+        this.balance = balance;
+    }
 
     public void setName(String name) {
         this.name = name;
     }
 
     public String getName() {
-        return name;
+        return this.name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount){
+        if(amount > 0){
+            this.balance = this.balance + amount;
+        }
+    }
+
+    public void withdrawal(double amount){
+        if(amount > this.balance){
+            this.balance = this.balance - amount;
+        }
     }
 }
--- src/FunWithAccounts.java
+++ src/FunWithAccounts.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class FunWithAccounts {
     public static void main(String[] args) {
 
         System.out.println("Fun with Accounts!");
 
-        Account account = new Account();
-        account.setName("Savings");
+        Account account = new Account("Noah Tuff", 0.00);
+        System.out.printf("\nAccount name: %s balance: $%.2f",
+                account.getName(), account.getBalance());
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an amount to deposit: ");
+        double amount = input.nextDouble();
+        account.deposit(amount);
+
+        account.setName("Savings");
         System.out.printf("Account name: %s", account.getName());
+
+        System.out.println("Enter an amount to withdraw: ");
+        double withdrawal = input.nextDouble();
+        account.withdrawal(amount);
+
     }
 }
